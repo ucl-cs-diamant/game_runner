984aa5f0a00cfceaa328df04badee1ea10c1fc29
Joel-Morgan <jojomorgan206@gmail.com>
2021-07-17 18:37:20 +0100

    deck unit tests done

--- game_engine_tests.py
+++ game_engine_tests.py
@@ -24,17 +24,39 @@ class CardTestCase(unittest.TestCase):
 
 class DeckTestCase(unittest.TestCase):
     def test_deck_generation_clean(self):
-        deck = game_engine.generate_deck(None)
-        self.assertEqual(len(deck), 35)
+        deck = game_engine.Deck()
+        self.assertEqual(len(deck.cards), 35)
 
-        relics = [card for card in deck if card.card_type == "Relic"]
+        relics = [card for card in deck.cards if card.card_type == "Relic"]
         self.assertEqual(len(relics), 5)
 
-        traps = [card for card in deck if card.card_type == "Trap"]
+        traps = [card for card in deck.cards if card.card_type == "Trap"]
         self.assertEqual(len(traps), 15)
 
-        treasures = [card for card in deck if card.card_type == "Treasure"]
+        treasures = [card for card in deck.cards if card.card_type == "Treasure"]
         self.assertEqual(len(treasures), 15)
+
+    def test_deck_generation_exclusion(self):
+        deck = game_engine.Deck([game_engine.Card("Relic", 5), game_engine.Card("Trap", "Snake")])
+        self.assertEqual(len(deck.cards), 33)
+
+        relics = [card for card in deck.cards if card.card_type == "Relic"]
+        self.assertEqual(len(relics), 4)
+
+        traps = [card for card in deck.cards if card.card_type == "Trap"]
+        self.assertEqual(len(traps), 14)
+
+        treasures = [card for card in deck.cards if card.card_type == "Treasure"]
+        self.assertEqual(len(treasures), 15)
+
+    def test_deck_pick_card(self):
+        deck = game_engine.Deck()
+        first_card = deck.cards[0]
+        second_card = deck.cards[1]
+        picked_card = deck.pick_card()
+
+        self.assertEqual(first_card, picked_card)
+        self.assertEqual(second_card, deck.cards[0])
 
 
 if __name__ == '__main__':
