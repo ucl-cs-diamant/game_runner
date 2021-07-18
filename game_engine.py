738b56378ab3024e638030489600d5a8c91fbc09
Joel-Morgan <jojomorgan206@gmail.com>
2021-07-18 19:50:59 +0100

    Changed to non async for testing, implemented fix for relics to be counted when placed, tests fixed to test for this.

--- game_engine.py
+++ game_engine.py
@@ -95,34 +95,41 @@ class Player:
     def _dispatch_action_request(self):  # todo: to implement
         pass
 
-    # def decide_action(self, ei):        # dummy roll 50/50 on leave/stay function
-    #
-    #     decision = np.random.randint(0, 2)
-    #     if decision:        # if 0 is rolled leave
-    #         self.continuing = True
-    #         return
-    #     self.continuing = False
+    def decide_action(self):        # dummy roll 50/50 on leave/stay function
+
+        decision = np.random.randint(0, 2)
+        if decision:        # if 0 is rolled leave
+            self.continuing = True
+            return
+        self.continuing = False
 
 
 class Board:
     def __init__(self):
         self.route = []
         self.double_trap = False
-        self.triggered_doubles = []
-        self.relics_picked = 0  # note: relics are only counted when actually collected by an explorer on the way out
+        self.excluded_cards = []
+        self.relics_picked = 0  # note: relics are counted when placed in the route
 
     def __str__(self):
         return str(self.route)
 
     # pick a card, if its another trap card, set double_trap to the trap card and kill the players at some point
     def add_card(self, card):
-        if card.card_type == "Trap":
+        if card.card_type == "Trap":    # Traps need to checked before being added for logic reasons
             for board_card in self.route:
                 if card.value == board_card.value:
                     self.double_trap = True
-                    self.triggered_doubles.append(card)
+                    self.excluded_cards.append(card)
                     break
+
         self.route.append(card)
+
+        if card.card_type == "Relic":   # Relics can be changed after adding neatly
+            self.relics_picked += 1
+            self.excluded_cards.append(card)
+            if self.relics_picked > 3:  # 4th and 5th relic have 10 value
+                self.route[-1].value = 10
 
     def reset_path(self):  # intentionally left out triggered doubles so it carries between paths
         self.route = []
@@ -154,14 +161,13 @@ def advancement_phase(path_deck, path_player_list, path_board):
     if no_active_players == 0:
         return True  # return immediately to move to the next expedition
 
-    # Not actually sure if python is able to do inline extraction of these list accesses
     last_route = path_board.route[-1]
 
     if last_route.card_type == "Treasure":
         handle_treasure_loot(last_route, active_players)
 
-    if last_route.card_type == "Relic":  # nothing extra is done when a relic is pulled
-        pass  # <-----------------  did you mean continue? or can this `if` be removed altogether?
+    if last_route.card_type == "Relic":  # this IF is here for sheer readability and does nothing
+        pass
 
     if last_route.card_type == "Trap":
         if path_board.double_trap:  # if its the second trap, kill all active players
@@ -172,13 +178,13 @@ def advancement_phase(path_deck, path_player_list, path_board):
         return False
 
 
-async def decision_phase(path_player_list, path_board, ei):
-    # for player in path_player_list:
-    #     if player.in_cave:
-    #         player.decide_action()
-    player_decisions = await ei.request_decisions()
-    for player_decision in player_decisions:
-        path_player_list[player_decision["player_id"]].continuing = player_decision["decision"]
+def decision_phase(path_player_list, path_board):
+    for player in path_player_list:
+        if player.in_cave:
+            player.decide_action()
+    # player_decisions = await ei.request_decisions()
+    # for player_decision in player_decisions:
+    #     path_player_list[player_decision["player_id"]].continuing = player_decision["decision"]
     # leaving players leaving and number of leaving players
     leaving_players = [player for player in path_player_list if player.in_cave and not player.continuing]
     no_leaving_players = len(leaving_players)
@@ -190,17 +196,11 @@ async def decision_phase(path_player_list, path_board, ei):
             if board_card.card_type == "Treasure":       # split loot evenly between players on treasure cards
                 handle_treasure_loot(board_card, leaving_players)
 
-            # <-- do we need to check that board_card.value != 0?
-            # Aren't relic always worth more than 0 even when the Card object is created?
-            if board_card.card_type == "Relic":
-                if no_leaving_players == 1 and board_card.value != 0:
+            # check if there is a relic to pick up (arguably pointless but it saves running the extra code 9/10 times)
+            if board_card.card_type == "Relic" and board_card.value != 0:
+                if no_leaving_players == 1:
                     for player in leaving_players:
-                        # increase the worth of a relic if its the last 2 from 5 to 10
-                        if path_board.relics_picked >= 3:
-                            player.pickup_loot(board_card.value * 2)
-                        else:
-                            player.pickup_loot(board_card.value)
-                        path_board.relics_picked += 1
+                        player.pickup_loot(board_card.value)
                         board_card.value = 0
 
             if board_card.card_type == "Trap":       # dont care about traps
@@ -211,29 +211,29 @@ async def decision_phase(path_player_list, path_board, ei):
         player.leave_cave()
 
 
-async def single_turn(path_deck, path_player_list, path_board, ei):
+def single_turn(path_deck, path_player_list, path_board):
     # advancement phase
     expedition_failed = advancement_phase(path_deck, path_player_list, path_board)
     if expedition_failed:   # propagate the failure up
         return True
     # decision phase
-    await decision_phase(path_player_list, path_board, ei)
+    decision_phase(path_player_list, path_board)
     return False
 
 
-async def run_path(deck, player_list, board, ei):     # runs through a path until all players leave or the run dies
+def run_path(deck, player_list, board):     # runs through a path until all players leave or the run dies
     path_complete = False
     while not path_complete:
-        path_complete = await single_turn(deck, player_list, board, ei)
+        path_complete = single_turn(deck, player_list, board)
     board.reset_path()      # reset board for a new path
 
 
-async def run_game(engine_interface):     # run a full game of diamant
+def run_game():     # run a full game of diamant
     deck, board = setup_game()
-    player_list = [Player(player_id) for player_id in engine_interface.players]
+    player_list = [Player(player_id) for player_id in range(6)]
     for path_num in range(5):      # do 5 paths
-        await run_path(deck, player_list, board, engine_interface)
-        excluded_cards = board.triggered_doubles
+        run_path(deck, player_list, board)
+        excluded_cards = board.excluded_cards
         for relic_count in range(board.relics_picked):        # add an exclusion for every picked relic
             excluded_cards.append(Card("Relic", 5))
         deck = Deck(excluded_cards)
@@ -269,9 +269,7 @@ async def run_game(engine_interface):     # run a full game of diamant
 
 
 async def main():
-    engine_interface = EngineInterface('localhost')  # todo: replace with environment variables
-    await engine_interface.init_game()
-    print(str(await run_game(engine_interface)) + " winner winner chicken dinner!")
+    print(str(run_game()) + " winner winner chicken dinner!")
 
 
 if __name__ == '__main__':
