459e25441dfb8fd56e711a1e44f401e22f2e3dc2
daohe <daohengliu@gmail.com>
2021-07-18 20:25:34 +0100

    move game interface into submodule
    Took 9 minutes

--- diamant_game_interface/__init__.py
+++
@@ -1,6 +0,0 @@
-from .PlayerInterface import PlayerInterface
-from .EngineInterface import EngineInterface
-
-__all__ = ['PlayerInterface', 'EngineInterface']
-
-# TODO: move this to a git repository and include this package as a submodule on git
